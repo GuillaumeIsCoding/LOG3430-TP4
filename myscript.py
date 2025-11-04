7c283633dbb578c60ade2f2f299801b0d1cd6a19
GuillaumeIsCoding <115158922+GuillaumeIsCoding@users.noreply.github.com>
2025-11-04 12:10:28 -0500

    Cooking time

--- myscript.py
+++ myscript.py
@@ -1,3 +1,8 @@
 import os
-os.system("git bisect start $badhash $goodhash")
-os.system("git bisect run python manage.py test")
+import subprocess
+
+bad = os.environ["BADHASH"]
+good = os.environ["GOODHASH"]
+
+subprocess.run(["git", "bisect", "start", bad, good])
+subprocess.run(["git", "bisect", "run", "python", "manage.py", "test"])
