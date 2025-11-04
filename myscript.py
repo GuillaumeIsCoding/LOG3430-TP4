f402dd710179474a630eff063a39cefb92428bc4
GuillaumeIsCoding <115158922+GuillaumeIsCoding@users.noreply.github.com>
2025-11-04 12:11:12 -0500

    ...

--- myscript.py
+++ myscript.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 
-bad = os.environ["BADHASH"]
-good = os.environ["GOODHASH"]
+bad = os.environ["badhash"]
+good = os.environ["goodhash"]
 
 subprocess.run(["git", "bisect", "start", bad, good])
 subprocess.run(["git", "bisect", "run", "python", "manage.py", "test"])
